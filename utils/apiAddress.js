0131f293e1a25838419ed398a56557bf776eda37
justinlee215 <justin.lee@pacificgroup.net>
2022-05-10 08:37:16 -0700

    Switch to SCSS

--- utils/apiAddress.js
+++ utils/apiAddress.js
@@ -1,12 +1,9 @@
 export let apiAddress;
-    
-if (process.env.NODE_ENV === 'productions') {
-    apiAddress = 'https://pcb365-4.vercel.app'
+
+if (process.env.NODE_ENV === "productions") {
+  apiAddress = "https://pcb365-4.vercel.app";
 } else {
-    // apiAddress = 'http://127.0.0.1'
-    // apiAddress = 'http://localhost:3000'
-    apiAddress = 'https://pcb365-4.vercel.app'
+  // apiAddress = 'http://127.0.0.1'
+  apiAddress = "http://localhost:3000";
+  // apiAddress = 'https://pcb365-4.vercel.app'
 }
-
-
-
