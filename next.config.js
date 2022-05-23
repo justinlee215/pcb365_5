04bfe19faeb650148a0e96f7578c8559009ce055
justinlee215 <justin.lee@pacificgroup.net>
2022-05-23 11:17:35 -0700

    Update corse config:

--- next.config.js
+++ next.config.js
@@ -2,16 +2,40 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['https://picsum.photos']
-  }
-}
+    domains: ["https://picsum.photos"],
+  },
+};
 
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
